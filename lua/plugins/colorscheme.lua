2b44a9c281fa70d0f6376e394501c8a97f338046
R3DST0RM <admin@r3d-soft.de>
2025-10-10 11:04:39 +0200

    Create colorscheme.lua

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- Themes
+  { "ellisonleao/gruvbox.nvim" },
+  { "Mofiqul/dracula.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
