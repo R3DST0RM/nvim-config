f224945cd981adeb595db8727d0e7b1bfc59b750
R3DST0RM <admin@r3d-soft.de>
2025-10-26 19:40:04 +0100

    Update and rename colorscheme.lua to dracula.lua
    Use opacity

---
+++ lua/plugins/dracula.lua
@@ -0,0 +1,33 @@
+return {
+  "Mofiqul/dracula.nvim",
+  priority = 1000,
+  opts = {
+    transparent_bg = true,
+    overrides = function()
+      local groups = {
+        "Normal",
+        "NormalNC",
+        "NormalFloat",
+        "FloatBorder",
+        "SignColumn",
+        "LineNr",
+        "EndOfBuffer",
+        "Pmenu",
+        "PmenuSel",
+        "TelescopeNormal",
+        "TelescopeBorder",
+        "CursorLine",
+      }
+      local o = {}
+      for _, g in ipairs(groups) do
+        o[g] = { bg = "NONE" }
+      end
+      o.PmenuSel.bold = true
+      return o
+    end,
+  },
+  config = function(_, opts)
+    require("dracula").setup(opts)
+    vim.cmd.colorscheme("dracula")
+  end,
+}
