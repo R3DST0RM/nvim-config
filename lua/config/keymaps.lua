21d54376dc4906573203f9b660890f609c12728c
Dominik Schwarzbauer <dominik.schwarzbauer@gmail.com>
2025-10-30 22:56:31 +0100

    feat: tmux-sessionizer keymaps and mini-snippets

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,10 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true })
 -- Normal mappings
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer", silent = true })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Previous buffer", silent = true })
+
+-- tmux-sessionizer mappings
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
+vim.keymap.set("n", "<M-t>", "<cmd>silent !tmux neww tmux-sessionizer -s 1<CR>")
+vim.keymap.set("n", "<M-n>", "<cmd>silent !tmux neww tmux-sessionizer -s 2<CR>")
+vim.keymap.set("n", "<M-s>", "<cmd>silent !tmux neww tmux-sessionizer -s 3<CR>")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -48,6 +48,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
+    { import = "lazyvim.plugins.extras.coding.mini-snippets" },
 
     -- Testing
     { import = "lazyvim.plugins.extras.test" },
