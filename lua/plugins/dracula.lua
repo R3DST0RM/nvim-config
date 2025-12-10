62f385121931d64a87b22a69addfe8cede55d812
Dominik Schwarzbauer <dominik.schwarzbauer@gmail.com>
2025-12-10 19:56:03 +0100

    disable transparency

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -1,31 +1,7 @@
 return {
   "Mofiqul/dracula.nvim",
   priority = 1000,
-  opts = {
-    transparent_bg = true,
-    overrides = function()
-      local groups = {
-        "Normal",
-        "NormalNC",
-        "NormalFloat",
-        "FloatBorder",
-        "SignColumn",
-        "LineNr",
-        "EndOfBuffer",
-        --"Pmenu",
-        --"PmenuSel",
-        "TelescopeNormal",
-        "TelescopeBorder",
-        "CursorLine",
-      }
-      local o = {}
-      for _, g in ipairs(groups) do
-        o[g] = { bg = "NONE" }
-      end
-      --o.PmenuSel.bold = true
-      return o
-    end,
-  },
+  opts = {},
   config = function(_, opts)
     require("dracula").setup(opts)
     vim.cmd.colorscheme("dracula")
