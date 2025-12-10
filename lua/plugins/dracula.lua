ad6fc4a71c3562a9619533ebc9d2cf49eb41de61
Dominik Schwarzbauer <dominik.schwarzbauer@gmail.com>
2025-12-10 20:39:56 +0100

    use rose-pine with transparency

--- lua/plugins/dracula.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Mofiqul/dracula.nvim",
-  priority = 1000,
-  opts = {},
-  config = function(_, opts)
-    require("dracula").setup(opts)
-    vim.cmd.colorscheme("dracula")
-  end,
-}
