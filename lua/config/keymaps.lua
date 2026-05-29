efc1925b3467e4e8bdab5c95702d3dadf896a6a6
R3DST0RM <admin@r3d-soft.de>
2026-05-29 10:04:57 +0200

    Add debugging key mappings for DAP integration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,3 +18,48 @@ vim.keymap.set("n", "<M-s>", "<cmd>silent !tmux neww tmux-sessionizer -s 3<CR>")
 
 -- Visual mappings
 vim.keymap.set("v", "<leader>csl", ":sort<CR>", { desc = "Sort lines", silent = true })
+
+-- Debugging mappings
+vim.keymap.set("n", "<leader>dv", function()
+  require("dapui").float_element("scopes", { enter = true })
+end, { desc = "DAP Scopes float" })
+
+vim.keymap.set({ "n", "v" }, "<leader>dx", function()
+  require("dapui").eval()
+end, { desc = "DAP Eval float" })
+
+vim.keymap.set("n", "<leader>dX", function()
+  require("dap").repl.open()
+
+  vim.defer_fn(function()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      local buf = vim.api.nvim_win_get_buf(win)
+
+      if vim.bo[buf].filetype == "dap-repl" then
+        local width = math.floor(vim.o.columns * 0.8)
+        local height = math.floor(vim.o.lines * 0.3)
+
+        vim.api.nvim_win_set_config(win, {
+          relative = "editor",
+          width = width,
+          height = height,
+          row = vim.o.lines - height - 2,
+          col = math.floor((vim.o.columns - width) / 2),
+          style = "minimal",
+          border = "rounded",
+        })
+
+        vim.api.nvim_set_current_win(win)
+
+        -- 🔥 IMPORTANT: q zum Schließen
+        vim.keymap.set("n", "q", function()
+          vim.api.nvim_win_close(win, true)
+        end, { buffer = buf, silent = true })
+
+        vim.keymap.set("n", "<Esc>", function()
+          vim.api.nvim_win_close(win, true)
+        end, { buffer = buf, silent = true })
+      end
+    end
+  end, 50)
+end, { desc = "Floating DAP REPL" })
