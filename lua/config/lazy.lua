b092d8b429b2e4ff5526c723236fa92e666638ed
R3DST0RM <dominik.schwarzbauer@gmail.com>
2026-08-17 07:52:08 +0200

    chore: add official biome lazy extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -44,6 +44,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.typescript.biome" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.zig" },
 
